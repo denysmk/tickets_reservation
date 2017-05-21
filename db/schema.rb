eaf91c07110d9e601f04ce0dd91901be90b2faba
Denys Makarenko <makarenko.denys@gmail.com>
2017-05-21 16:18:40 +0300

    Add postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,13 @@
 
 ActiveRecord::Schema.define(version: 20170519125440) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "roles", force: :cascade do |t|
     t.string "name"
     t.string "resource_type"
-    t.integer "resource_id"
+    t.bigint "resource_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
@@ -29,8 +32,8 @@ ActiveRecord::Schema.define(version: 20170519125440) do
     t.integer "seat_number"
     t.string "first_name"
     t.string "last_name"
-    t.integer "user_id"
-    t.integer "trip_id"
+    t.bigint "user_id"
+    t.bigint "trip_id"
     t.index ["trip_id"], name: "index_tickets_on_trip_id"
     t.index ["user_id"], name: "index_tickets_on_user_id"
   end
@@ -63,11 +66,13 @@ ActiveRecord::Schema.define(version: 20170519125440) do
   end
 
   create_table "users_roles", id: false, force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "role_id"
+    t.bigint "user_id"
+    t.bigint "role_id"
     t.index ["role_id"], name: "index_users_roles_on_role_id"
     t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
     t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
+  add_foreign_key "tickets", "trips"
+  add_foreign_key "tickets", "users"
 end
